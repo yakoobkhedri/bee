475729a8ea65193efb27cb6e8529af15d507d779
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-12-12 00:31:32 +0330

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,70 @@
+// dropdown menu
+
+let productMenu = document.getElementById('productMenu');
+let dropdowns = document.querySelector('.dropdowns');
+let dropdown1 = Array.from(document.querySelectorAll('.dropdown1 > li'));
+let dropdown2 = Array.from(document.querySelectorAll('.dropdown2 > li'));
+let accordionBtn = Array.from(document.getElementsByClassName('accordionBtn'));
+let hamIcon = document.getElementById('hamIcon');
+let mobileMenu = document.getElementById('mobileMenu');
+let languageLi = document.querySelector('.nav-item.dropdown');
+
+hamIcon.addEventListener('click', function () {
+  mobileMenu.classList.toggle('active');
+})
+
+accordionBtn.forEach((item) => {
+  item.addEventListener('click', function () {
+    item.nextElementSibling.classList.toggle('active');
+    item.querySelector('.bi-chevron-up').classList.toggle('active');
+  })
+})
+productMenu.addEventListener('mouseover', function () {
+  this.parentElement.nextElementSibling.classList.add('active');
+})
+
+dropdown1.forEach((item) => {
+  item.addEventListener('mouseover', function () {
+    dropdown1.forEach((li) => li.classList.remove('active'));
+    item.classList.add('active');
+    let tabId = item.dataset.id;
+    dropdown2.forEach((content) => {
+      let contentId = content.dataset.id;
+      if (tabId === contentId) {
+        content.classList.add('active');
+      } else {
+        content.classList.remove('active');
+      }
+    })
+  })
+});
+dropdowns.addEventListener('mouseleave', function () {
+  dropdowns.classList.remove('active');
+})
+
+// swiper
+var banner = new Swiper(".banner", {
+  loop: true,
+  autoplay: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// filter
+
+let filterBtn = Array.from(document.getElementsByClassName('filterBtn'));
+let filterContent = Array.from(document.getElementsByClassName('filterContent'));
+
+filterBtn.forEach((item) => {
+  item.addEventListener('click', function () {
+    filterBtn.forEach((items) => {
+      items.nextElementSibling.classList.remove('active');
+      items.querySelector('svg').classList.remove('active');
+    })
+    item.nextElementSibling.classList.add('active');
+    item.querySelector('svg').classList.add('active');
+  })
+})
